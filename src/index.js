b22569ddc4ca5045b55189d2ad79f29f7eda7f75
Roman <mail.ru>
2023-01-16 21:28:24 +0300

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App/>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+//Описание того как работает bindActionCreators 
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+
+
+
+// document.getElementById('inc').addEventListener('click', inc);
+
+
+// document.getElementById('dec').addEventListener('click', dec);
+
+
+// document.getElementById('rnd').addEventListener('click', () => {
+//     const value = Math.floor(Math.random() * 10);
+//     rnd(value);
+//   });
+
+
+
+
+
+
+
+
+
 
 
